cde0ad1433b7d9824572ff3aab1bb34736dc89e5
reza karder <100065572+ractod@users.noreply.github.com>
2023-04-19 23:43:02 +0430

    base url value is changed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = process.env.REACT_APP_NODE_ENV == "development" ? "http://localhost:5000" : process.env.REACT_APP_BASE_URL
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
